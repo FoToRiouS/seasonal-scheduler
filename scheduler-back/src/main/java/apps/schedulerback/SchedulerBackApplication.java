2d1bca53bc442894b6efa13cfc116fefb8e53ca8
fernando.slopes.21@gmail.com <Pororoc@21>
2023-06-28 21:31:42 -0300

    Commit Inicial

---
+++ scheduler-back/src/main/java/apps/schedulerback/SchedulerBackApplication.java
@@ -0,0 +1,13 @@
+package apps.schedulerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerBackApplication.class, args);
+    }
+
+}
---
+++ scheduler-back/src/test/java/apps/schedulerback/SchedulerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package apps.schedulerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
