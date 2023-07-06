6841321107fe0e327409dfc9c5ce469923781f4f
fernando.slopes.21@gmail.com <Pororoc@21>
2023-07-06 00:03:51 -0300

    Estudando

--- scheduler-front/src/app/shared/services/api/animes/AnimesService.ts
+++ scheduler-front/src/app/shared/services/api/animes/AnimesService.ts
@@ -2,10 +2,13 @@ import {MyAnimeListApi} from "../MyAnimeListApi.ts";
 import {ApiException} from "../ApiException.ts";
 
 export interface IAnime {
+    id: number,
     title: string,
     alternativeTitles: IAlternativeTitles,
     mainPicture: IMainPicture,
-    broadcast: IBroadcast
+    broadcast: IBroadcast,
+    startSeason: IStartSeason,
+    mediaType: "tv" | "ona" | "ova" | "movie" | "special"
 }
 
 interface IAlternativeTitles {
@@ -18,28 +21,41 @@ interface IMainPicture {
 }
 
 interface IBroadcast {
-    "day_of_the_week": string,
+    day_of_the_week: "sunday",
     start_time: string
+}
+
+interface IStartSeason {
+    year: number,
+    season: "winter" | "spring" | "summer" | "fall"
 }
 
 const headers = {"X-MAL-CLIENT-ID": "eb6aa17ec9b6961f8812f79c38318240"}
 
+const mapJsonAnime = (node: any) => {
+    return <IAnime> {
+        id: node.node.id,
+        title: node.node.title,
+        alternativeTitles: node.node.alternative_titles,
+        mainPicture: node.node.main_picture,
+        broadcast: node.node.broadcast,
+        mediaType: node.node.media_type,
+        startSeason: node.node.start_season
+    }
+}
+
 const getBySeason = async (year: number, season: "winter" | "spring" | "summer" | "fall") : Promise<IAnime[] | ApiException> => {
     try{
-        const { data } = await MyAnimeListApi().get(`/anime/season/${year}/${season}?limit=500&fields=alternative_titles,broadcast`, {
+        const { data } = await MyAnimeListApi().get(`/anime/season/${year}/${season}?limit=500&fields=alternative_titles,broadcast,media_type,start_season`, {
             headers: headers
         });
 
-        return data.data.map((node: any) => {
-            return <IAnime> {
-                title: node.node.title,
-                alternativeTitles: node.node.alternative_titles,
-                mainPicture: node.node.main_picture,
-                broadcast: node.node.broadcast
-            }
-        }).sort((a1:IAnime, a2:IAnime) => {
-            return a1.title.localeCompare(a2.title);
-        });
+        return data.data.map(mapJsonAnime)
+            .filter((a:IAnime) => a.mediaType === "tv" || a.mediaType === "ona" || a.mediaType === "ova")
+            .filter((a:IAnime) => a.startSeason.year === year && a.startSeason.season === season)
+            .sort((a1:IAnime, a2:IAnime) => {
+                return a1.title.localeCompare(a2.title);
+            });
     } catch (e: any) {
         return new ApiException(e.message || "Erro ao consultar API!")
     }
@@ -50,18 +66,7 @@ const getById = async (id: number) : Promise<IAnime | ApiException> => {
         const { data } = await MyAnimeListApi().get(`/anime/${id}`, {
             headers: headers
         });
-        return data;
-    } catch (e: any) {
-        return new ApiException(e.message || "Erro ao consultar API!")
-    }
-}
-
-const getAll = async () : Promise<IAnime[] | ApiException> => {
-    try{
-        const { data } = await MyAnimeListApi().get("/anime",{
-            headers: headers
-        });
-        return data;
+        return data.data.map(mapJsonAnime);
     } catch (e: any) {
         return new ApiException(e.message || "Erro ao consultar API!")
     }
@@ -70,5 +75,4 @@ const getAll = async () : Promise<IAnime[] | ApiException> => {
 export const AnimesService = {
     getBySeason,
     getById,
-    getAll
 }
