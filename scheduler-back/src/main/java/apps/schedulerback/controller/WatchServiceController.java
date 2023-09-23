5c97468a824eb2fe1778bac0d5bcd131d9e6bc02
Fernando Lopes <fernando.slopes.21@gmail.com>
2023-09-22 22:17:09 -0300

    Preparação dos arquivos do Docker para deploy

--- scheduler-back/src/main/java/apps/schedulerback/controller/WatchServiceController.java
+++ scheduler-back/src/main/java/apps/schedulerback/controller/WatchServiceController.java
@@ -1,11 +1,8 @@
 package apps.schedulerback.controller;
 
-import apps.schedulerback.model.Season;
-import apps.schedulerback.model.WatchService;
 import apps.schedulerback.model.record.WatchServiceResponse;
 import apps.schedulerback.service.AnimeSeasonService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +19,6 @@ public class WatchServiceController {
         this.animeSeasonService = animeSeasonService;
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(value = "/list", produces = "application/json")
     public ResponseEntity<Collection<WatchServiceResponse>> listAll(){
         return ResponseEntity.ok(animeSeasonService.listAllWatchServices().stream().map(WatchServiceResponse::new).toList());
