58668a52cffe41e3427ee14a296c2da09dd078d3
Fernando Lopes <fernando.slopes.21@gmail.com>
2023-09-24 00:00:20 -0300

    Preparação dos arquivos do Docker para deploy

--- scheduler-front/src/app/shared/services/api/TelegramApi.ts
+++ scheduler-front/src/app/shared/services/api/TelegramApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const TelegramApi = () => {
+    const token = import.meta.env.VITE_TELEGRAM_BOT_TOKEN;
+
     return axios.create({
         // baseURL: import.meta.env.MODE === "development" ? "/telegram" : import.meta.env.VITE_TELEGRAM_API,
-        baseURL: "/telegram"
+        baseURL: `/telegram/bot${token}`
     });
 }
