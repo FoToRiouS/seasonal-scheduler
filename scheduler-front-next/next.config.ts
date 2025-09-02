bffa682e9a08563ec5cacd045d2b291256e8a5d8
Fernando Lopes <fernando.lopes@unievangelica.edu.br>
2025-09-02 14:59:10 -0300

    ✨ feat(nextjs): Adiciona allowedOrigins para serverActions para permitir requisições de localhost e do IP do servidor.

--- scheduler-front-next/next.config.ts
+++ scheduler-front-next/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: "standalone",
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["localhost:3000", "193.181.209.159"],
+        },
+    },
 };
 
 export default nextConfig;
