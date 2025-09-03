3e8a2b8870b950d48b97def4717efc8b2eef67cf
Fernando Lopes <fernando.lopes@unievangelica.edu.br>
2025-09-03 11:19:01 -0300

    ✨ feat(docker): Monta volume do Let's Encrypt para os certificados SSL ✨ feat(nginx): Adiciona configurações de SSL para Let's Encrypt 🔥 chore(nextjs): Remove allowedOrigins para serverActions

--- scheduler-front-next/next.config.ts
+++ scheduler-front-next/next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: "standalone",
-    experimental: {
-        serverActions: {
-            allowedOrigins: ["localhost:3000", "193.181.209.159"],
-        },
-    },
 };
 
 export default nextConfig;
